f8f703d567721ff3533c273d4488a8d0e28853e5
Henry <cryptix@riseup.net>
2013-11-29 19:42:30 +0100

    factored out canvas to its own package

--- server.go
+++ server.go
@@ -6,14 +6,16 @@ import (
 	"image"
 	"image/png"
 	"io"
+	"log"
 	"net/http"
 	"time"
 
 	"code.google.com/p/go.net/websocket"
+	"github.com/cryptix/canvas"
 	"github.com/gorilla/mux"
 )
 
-func MakeGradientHandler(canvas *Canvas, tick <-chan bool) websocket.Handler {
+func MakeGradientHandler(canvas *canvas.Canvas, tick <-chan bool) websocket.Handler {
 
 	return func(ws *websocket.Conn) {
 		for {
@@ -23,7 +25,9 @@ func MakeGradientHandler(canvas *Canvas, tick <-chan bool) websocket.Handler {
 			imgBuf := new(bytes.Buffer)
 			imgEncoder := base64.NewEncoder(base64.StdEncoding, imgBuf)
 
+			// jpeg.Encode(imgEncoder, canvas, nil)
 			png.Encode(imgEncoder, canvas)
+
 			imgEncoder.Close()
 
 			io.Copy(ws, imgBuf)
@@ -37,7 +41,7 @@ func main() {
 
 	width, height := 640, 640
 
-	canvas := NewCanvas(image.Rect(0, 0, width, height))
+	canvas := canvas.NewCanvas(image.Rect(0, 0, width, height))
 
 	n := 150
 	world := NewWorld(n, 6, canvas)
@@ -58,7 +62,7 @@ func main() {
 
 			world.SendMessages(1)
 
-			time.Sleep(500 * time.Millisecond)
+			time.Sleep(250 * time.Millisecond)
 			tick <- true
 
 		}
@@ -70,5 +74,6 @@ func main() {
 	staticHandler := http.FileServer(http.Dir("."))
 	router.PathPrefix("/").Handler(staticHandler)
 
+	log.Println("Listening on :3001")
 	http.ListenAndServe("localhost:3001", router)
 }
